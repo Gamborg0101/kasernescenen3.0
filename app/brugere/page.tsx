54518ef8933d972ffde290a606e808b9a2a8a7d6
Casper Gamborg <Gamborgc@gmail.com>
2026-02-06 22:07:52 +0100

    drunk

--- app/brugere/page.tsx
+++ app/brugere/page.tsx
@@ -1,7 +1,8 @@
 import { prisma } from '@/db';
 import BrugerTabel from './BrugerTabel';
+import { JSX } from 'react';
 
-export default async function Brugere() {
+export default async function Brugere(): Promise<JSX.Element> {
   const users = await prisma.user.findMany();
 
   console.log('Data fetched from database:', users);
