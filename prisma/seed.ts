f55a3f3f81c674fb299a01b0394723ae6440f50d
Casper Gamborg <Gamborgc@gmail.com>
2026-02-06 12:05:35 +0100

    prisma setup + seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,47 +1,67 @@
 import { PrismaClient } from '../app/generated/prisma';
-const prisma = new PrismaClient();
+import { PrismaPg } from '@prisma/adapter-pg';
+import { faker } from '@faker-js/faker';
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const prisma = new PrismaClient({
+  adapter,
+});
 
 async function main() {
-  const casper = await prisma.user.upsert({
-    where: { email: 'casper@example.com' },
-    update: {},
-    create: {
-      name: 'Casper',
-      email: 'casper@example.com',
-      password: 'securedpassword',
-      note: 'Admin',
-      category: 'Admin',
-    },
-  });
-  const room = await prisma.room.upsert({
-    where: { name: '114' },
-    update: {},
-    create: {
-      name: '114',
-      capacity: 20,
-      location: '1584-114',
-    },
-  });
-  const booking = await prisma.booking.upsert({
-    where: { id: 1 },
-    update: {},
-    create: {
-      bookingId: '1',
-      userId: casper.id,
-      roomId: room.id,
-      startTime: new Date(),
-      endTime: new Date(new Date().setHours(new Date().getHours() + 1)),
-    },
-  });
-  console.log({ casper, room, booking });
+  for (let i = 0; i < 10; i++) {
+    const user = await prisma.user.create({
+      data: {
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        note: faker.lorem.sentence(),
+        category: faker.helpers.arrayElement([
+          'dramaturgi',
+          'musikvidenskab',
+          'æstetik og kultur',
+          'retorik',
+        ]),
+      },
+    });
+    const room = await prisma.room.create({
+      data: {
+        name: faker.helpers.arrayElement([
+          '114',
+          '115',
+          '116',
+          '117',
+          '119',
+          '122',
+          '211',
+          '212',
+          '213',
+          '214',
+          '215',
+          '216',
+        ]),
+        capacity: faker.helpers.arrayElement([10, 20, 30, 40, 50]),
+        location: faker.helpers.arrayElement([
+          '1. Etage',
+          '2. Etage',
+          '3. Etage',
+        ]),
+      },
+    });
+    await prisma.booking.create({
+      data: {
+        bookingId: faker.string.uuid(),
+        userId: faker.helpers.arrayElement([user.id]),
+        roomId: faker.helpers.arrayElement([room.id]),
+        startTime: faker.date.soon(),
+        endTime: faker.date.soon({ days: 1 }),
+      },
+    });
+  }
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+  .catch(console.error)
+  .finally(() => prisma.$disconnect());
