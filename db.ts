a6ac1258c1b1dd6145964a200ddad106171f41de
Casper Gamborg <Gamborgc@gmail.com>
2026-02-19 11:41:45 +0100

    proxy is working for booking and brugere

--- db.ts
+++ db.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from './generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
-const adapter = new PrismaPg({
+export const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
 });
 
--- proxy.ts
+++ proxy.ts
@@ -1,13 +1,9 @@
 export { auth as proxy } from '@/auth/authSetup';
 
-// import { auth } from '@/auth/authSetup';
+// export const config = {
+//   matcher: ['/((?!api|_next/static|opret|_next/image|favicon.ico).*)'],
+// };
 
-// export const proxy = auth((req) => {
-//   if (!req.auth && req.nextUrl.pathname !== '/login') {
-//     const newUrl = new URL('/login', req.nextUrl.origin);
-//     return Response.redirect(newUrl);
-//   }
-// });
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/booking', '/brugere'],
 };
