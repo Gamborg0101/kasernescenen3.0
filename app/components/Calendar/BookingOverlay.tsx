5c81362262a49e9cb6a66ad3075bc546a923a738
Casper Gamborg <Gamborgc@gmail.com>
2026-03-10 09:13:01 +0100

    Bookingoverlay refactored

--- app/components/Calendar/BookingOverlay.tsx
+++ app/components/Calendar/BookingOverlay.tsx
@@ -1,3 +1,5 @@
+import { differenceInMinutes } from 'date-fns';
+
 type Booking = {
   id: number;
   roomId: number;
@@ -6,29 +8,31 @@ type Booking = {
   reason: string;
 };
 
-import { differenceInMinutes } from 'date-fns';
-
 type BookingOverlay = {
   bookings: Booking[];
 };
 
 export default function BookingOverlay({ bookings }: BookingOverlay) {
+  function getDivStartPosition(booking: Booking) {
+    const divStartPosition =
+      (booking.startTime.getHours() - 7) * 60 + booking.startTime.getMinutes();
+    return (divStartPosition / 15) * 20;
+  }
+
+  function getDivHeight(booking: Booking) {
+    return (differenceInMinutes(booking.endTime, booking.startTime) / 15) * 20;
+  }
+
   return (
     <div className="absolute w-full">
       {bookings.map((booking, index) => {
-        const divHeight =
-          (differenceInMinutes(booking.endTime, booking.startTime) / 15) * 20;
-
-        const divStartPosition =
-          (booking.startTime.getHours() - 7) * 60 +
-          booking.startTime.getMinutes();
-
-        const divStartPixels = (divStartPosition / 15) * 20;
-
         return (
           <div
             key={index}
-            style={{ height: `${divHeight}px`, top: `${divStartPixels}px` }}
+            style={{
+              height: `${getDivHeight(booking)}px`,
+              top: `${getDivStartPosition(booking)}px`,
+            }}
             className="absolute w-full bg-blue-500"
           >
             <p> Hi there</p>
