786d0c4a58ccac0f581d50b78acc535bb4a56ab9
Casper Gamborg <Gamborgc@gmail.com>
2026-04-20 11:23:28 +0200

    fixed de / dk bug

--- app/booking/BookingActions.tsx
+++ app/booking/BookingActions.tsx
@@ -14,7 +14,10 @@ function convertStartAndEndHour(
   getDate: string,
 ): { start: Date; end: Date } {
   const endTimeWithMins = `${endHour}:${endHourMins}`;
-  const startTime = new Date(`${getDate} ${startHour}`);
+
+  const startFormatted = `${startHour.split('.')[0]}:${startHour.split('.')[1]}`;
+
+  const startTime = new Date(`${getDate} ${startFormatted}`);
   const endTime = new Date(`${getDate} ${endTimeWithMins}`);
 
   return { start: startTime, end: endTime };
