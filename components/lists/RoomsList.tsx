25e80b75dd0afae05d668277084640ff86f2fab4
Casper Gamborg <Gamborgc@gmail.com>
2026-04-30 14:07:31 +0200

    Bug fixed

--- components/lists/RoomsList.tsx
+++ components/lists/RoomsList.tsx
@@ -1,14 +1,14 @@
 import { Room } from '@/generated/prisma';
-export default function RoomsList({ rooms }: { rooms: Room }) {
-  console.log(rooms);
+export default function RoomsList({ rooms }: { rooms: Room[] }) {
   return (
     <div>
-      {/* {rooms.map((item, index) => {
+      {rooms.map((item, index) => (
         <div key={index}>
-            {item.}
-        </div>;
-      })}
-      <p>These are the rooms</p> */}
+          {item.id} {/* or any field */}
+        </div>
+      ))}
+      <p>These are the rooms</p>
     </div>
   );
 }
+
