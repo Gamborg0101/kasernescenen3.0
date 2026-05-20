a4462d5f7a8829026a168600a3464dc6d8116b8c
Casper Gamborg <Gamborgc@gmail.com>
2026-05-20 11:17:34 +0200

    Booking logic updated

--- components/admin/PanelElement.tsx
+++ components/admin/PanelElement.tsx
@@ -16,7 +16,7 @@ export default function PanelElement({
   return (
     <div className="flex items-center flex-col justify-between h-60 w-60 bg-white border border-gray-300 shadow-sm rounded-2xl mt-20">
       <div className="mt-4 font-bold text-xl text-gray-900">
-        <p> {title}</p>
+        <p>{title}</p>
       </div>
       <p className="flex justify-center items-center text-sm p-3 text-ellipsis text-gray-500">{body}</p>
       <div className="mb-10">
