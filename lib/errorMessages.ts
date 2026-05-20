1076838cb5cef99cbe22cd6bda31ee498c7d65d6
Casper Gamborg <Gamborgc@gmail.com>
2026-05-20 16:35:29 +0200

    added contract to error messages

--- lib/errorMessages.ts
+++ lib/errorMessages.ts
@@ -1,64 +1,69 @@
-export const sessionError = {
+interface Message {
+  success: boolean;
+  error: string | null;
+}
+
+export const sessionError: Message = {
   success: false,
   error: 'Der er ikke en aktiv session',
 };
 
-export const ratelimitError = {
+export const ratelimitError: Message = {
   success: false,
   error: 'Ratelimit reached',
 };
 
-export const failedToCreateBooking = {
+export const failedToCreateBooking: Message = {
   success: false,
   error: 'Noget gik galt imens du forsøgte at lave en booking',
 };
-export const failedToDeleteBooking = {
+export const failedToDeleteBooking: Message = {
   success: false,
   error: 'Noget gik galt imens du forsøgte at slette en booking',
 };
 
-export const failedToCleanupDb = {
+export const failedToCleanupDb: Message = {
   success: false,
   error: 'Noget gik galt imens du forsøgte at lave en oprydning af databasen',
 };
 
-export const failedToUpdateRoom = {
+export const failedToUpdateRoom: Message = {
   success: false,
   error: 'Noget gik galt imens du forsøgte at opdatere rummet',
 };
 
-export const failedToDeleteRoom = {
+export const failedToDeleteRoom: Message = {
   success: false,
   error: 'Noget gik galt imens du forsøgte at slette et rum',
 };
 
-export const failedToCreateRoom = {
+export const failedToCreateRoom: Message = {
   success: false,
   error: 'Noget gik galt imens du forsøgte at lave et rum',
 };
 
-export const failedToCreateUser = {
+export const failedToCreateUser: Message = {
   success: false,
   error: 'Noget gik galt imens du forsøgte at oprette en bruger',
 };
 
-export const failedToDeleteUser = {
+export const failedToDeleteUser: Message = {
   success: false,
   error: 'Noget gik galt imens du forsøgte at slette en bruger',
 };
 
-export const failedToUpdateUser = {
+export const failedToUpdateUser: Message = {
   success: false,
   error: 'Noget gik galt imens du forsøgte at opdatere en bruger',
 };
 
-export const userIsNotAdmin = {
+export const userIsNotAdmin: Message = {
   success: false,
   error: 'Noget gik galt imens du forsøgte at slette en bruger',
 };
 
-export const unauthorizedAccess = { success: false, error: 'Unauthorized' };
+export const unauthorizedAccess: Message = { success: false, error: 'Unauthorized' };
 
-export const failedToFindRoom = { success: false, error: 'Noget gik galt med at finde rummet' };
+export const failedToFindRoom: Message = { success: false, error: 'Noget gik galt med at finde rummet' };
 
-export const success = { success: true, error: null };
+export const success: Message = { success: true, error: null };
