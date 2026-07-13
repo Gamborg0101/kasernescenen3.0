529c3ce84438fa0a92152b9818c5cf94e1b4c193
Casper Gamborg <Gamborgc@gmail.com>
2026-07-13 16:09:27 +0200

    Updated convertStartAndEndhour so it is worked properly

--- lib/utils/convertStartAndEndHour.ts
+++ lib/utils/convertStartAndEndHour.ts
@@ -4,12 +4,23 @@ export function convertStartAndEndHour(
   endHourMins: string,
   getDate: string,
 ): { start: Date; end: Date } {
+  const startFormatted = `${startHour.split('.')[0]}:${startHour.split('.')[1]}`;
   const endTimeWithMins = `${endHour}:${endHourMins}`;
 
-  const startFormatted = `${startHour.split('.')[0]}:${startHour.split('.')[1]}`;
+  const date = new Date(getDate);
 
-  const startTime = new Date(`${getDate} ${startFormatted}`);
-  const endTime = new Date(`${getDate} ${endTimeWithMins}`);
+  const startTime = new Date(date);
+  startTime.setHours(Number(startFormatted.split(':')[0]), Number(startFormatted.split(':')[1]));
+
+  const endTime = new Date(date);
+  endTime.setHours(Number(endTimeWithMins.split(':')[0]), Number(endTimeWithMins.split(':')[1]));
+  console.log({
+    getDate,
+    startFormatted,
+    endTimeWithMins,
+    startTime,
+    endTime,
+  });
 
   return { start: startTime, end: endTime };
 }
